481de8b14fd0a9b3eaa971c69af3d4c5c6daa5f6
BryanDietrichBernhardt <bryandbernhardt@gmail.com>
2023-01-18 23:11:41 -0300

    fix: ajust summary table justify center

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
+import "./styles/global.css";
 import { Header } from "./components/Header";
 import { SummaryTable } from "./components/SummaryTable";
-import "./styles/global.css";
 
 // import { Habit } from "./components/Habit";
 
